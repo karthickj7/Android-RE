0e8c7dc89b235f44a87b34eedf8ae71c4bc7c28a
Karthick Saran <96378805+karthickj7@users.noreply.github.com>
2025-11-11 15:56:06 +0530

    Add decryption script for Emoji Lab app

---
+++ FridaScripts/Others/com.genmojistudio.emojilab.js
@@ -0,0 +1,117 @@
+Java.perform(() => {
+    var decrypt = Java.use("q8.u");
+    console.log('("amE3dqE8", "VhVfH9ICoFA="); ---> ' + decrypt.a('amE3dqE8\n', 'VhVfH9ICoFA=\n'));
+    console.log('("COnCTTAW", "NJ2qJEMo4uc="); ---> ' + decrypt.a("COnCTTAW\n", "NJ2qJEMo4uc=\n"));
+    console.log('("COnCTTAW", "NJ2qJEMo4uc="); ---> ' + decrypt.a("COnCTTAW\n", "NJ2qJEMo4uc=\n"));
+    console.log('("ujAIpnh+Ig==", "ggc6l0FGFSQ="); ---> ' + decrypt.a("ujAIpnh+Ig==\n", "ggc6l0FGFSQ=\n"));
+    console.log('("TTnQWohsC2lf", "LF+PKfwNfxw="); ---> ' + decrypt.a("TTnQWohsC2lf\n", "LF+PKfwNfxw=\n"));
+    console.log('("3XfcZGNPWg==", "kgW7BQ0mOS8="); ---> ' + decrypt.a("3XfcZGNPWg==\n", "kgW7BQ0mOS8=\n"));
+    console.log('(("q293bGuN", "wAUWHw//KyU=")); ---> ' + decrypt.a("q293bGuN\n", "wAUWHw//KyU=\n"));
+    console.log('("f+dYxNWWVr5G+VrU0YE=", "GYAyp7PkIck="); ---> ' + decrypt.a("f+dYxNWWVr5G+VrU0YE=\n", "GYAyp7PkIck=\n"));
+    console.log('("YDdZ8WQTwS5gNw==", "DVItmQt3j08="); ---> ' + decrypt.a("YDdZ8WQTwS5gNw==\n", "DVItmQt3j08=\n"));
+    console.log('("wy232GUQqULBGLzJbQY=", "s0zFuQh13Sc="); ---> ' + decrypt.a("wy232GUQqULBGLzJbQY=\n", "s0zFuQh13Sc=\n"));
+    console.log('("T9fJh9qA", "Obal8r/zc78="); ---> ' + decrypt.a("T9fJh9qA\n", "Obal8r/zc78=\n"));
+    console.log('("UoZyU3aTVpJejHJTUJ9bxXOSfhNh", "POIafRL7Mbw="); ---> ' + decrypt.a("UoZyU3aTVpJejHJTUJ9bxXOSfhNh\n", "POIafRL7Mbw=\n"));
+    console.log('("XzWb1WeZOdU=", "PlbvvBHwTaw="); ---> ' + decrypt.a("XzWb1WeZOdU=\n", "PlbvvBHwTaw=\n"));
+    console.log('("8QA43fWQpGHxGiCRt5blbP4GIJGhnOVh8Bt536CfqS/rDCTU9ZKra+0aPdX7krV/sTQ3xbyFrHvmODXftJSgfQ==", "n3VUsdXzxQ8=")); ---> ' + decrypt.a("8QA43fWQpGHxGiCRt5blbP4GIJGhnOVh8Bt536CfqS/rDCTU9ZKra+0aPdX7krV/sTQ3xbyFrHvm\nODXftJSgfQ==\n", "n3VUsdXzxQ8=\n"));
+    console.log('("D1OQvFtadHsGUbeLXEJzcQ1FzMAAGjM=", "aDbk7i40GhI="); ---> ' + decrypt.a("D1OQvFtadHsGUbeLXEJzcQ1FzMAAGjM=\n", "aDbk7i40GhI=\n"));
+    console.log('("0OVeSAs00A==", "o4AsPmJXtTE="); ---> ' + decrypt.a("0OVeSAs00A==\n", "o4AsPmJXtTE=\n"));
+    console.log('("ibswbh06WiaJ9moTR2E=", "7t5EPWlIM0g="); ---> ' + decrypt.a("ibswbh06WiaJ9moTR2E=\n", "7t5EPWlIM0g=\n"));
+    console.log('("lkBiBpTx9lLaHA==", "9DULavDZ2Hw="); ---> ' + decrypt.a("lkBiBpTx9lLaHA==\n", "9DULavDZ2Hw=\n"));
+    console.log('("dPbgCQ==", "F5eMZeFIDrY="); ---> ' + decrypt.a("dPbgCQ==\n", "F5eMZeFIDrY=\n"));
+    console.log('("vgknL1EDmh/yVQ==", "3HxOQzUrtDE="); ---> ' + decrypt.a("vgknL1EDmh/yVQ==\n", "3HxOQzUrtDE=\n"));
+    console.log('("UoZyU3aTVpJejHJTUJ9bxXOSfhNh", "POIafRL7Mbw=") ---> ' + decrypt.a("UoZyU3aTVpJejHJTUJ9bxXOSfhNh\n", "POIafRL7Mbw=\n"));
+    console.log('("XzWb1WeZOdU=", "PlbvvBHwTaw=") ---> ' + decrypt.a("XzWb1WeZOdU=\n", "PlbvvBHwTaw=\n"));
+    console.log('("D1OQvFtadHsGUbeLXEJzcQ1FzMAAGjM=", "aDbk7i40GhI=") ---> ' + decrypt.a("D1OQvFtadHsGUbeLXEJzcQ1FzMAAGjM=\n", "aDbk7i40GhI=\n"));
+    console.log('("ZLUR2jUmRVB5pwnJMWMvRniXEc0vPidMYbAY3g==", "ANR9rFxNayM=") ---> ' + decrypt.a("ZLUR2jUmRVB5pwnJMWMvRniXEc0vPidMYbAY3g==\n", "ANR9rFxNayM=\n"));
+    console.log('("w/0h5ymh9JvA7jPsA7bMjts=", "rZ9WglzCq+w=") ---> ' + decrypt.a("w/0h5ymh9JvA7jPsA7bMjts=\n", "rZ9WglzCq+w=\n"));
+    console.log('("6NfM+S/LclzrxN7yBc1JSOA=", "hrW7nFqoLSs=") ---> ' + decrypt.a("6NfM+S/LclzrxN7yBc1JSOA=\n", "hrW7nFqoLSs=\n"));
+    console.log('("upUB2YTjyhKrmxGl8vHoWZ+5PJE=", "+9BS9sehiT0=") ---> ' + decrypt.a("upUB2YTjyhKrmxGl8vHoWZ+5PJE=\n", "+9BS9sehiT0=\n"));
+    console.log('("UOgD", "Ea1Q/ZFlymQ=")) ---> ' + decrypt.a("UOgD\n", "Ea1Q/ZFlymQ=\n"));
+    console.log('("aSC+c9H/bQ==", "BlfUEb+KHxI=") ---> ' + decrypt.a("aSC+c9H/bQ==\n", "BlfUEb+KHxI=\n"));
+    console.log('("ZLUR2jUmRVB5pwnJMWMvRniXEc0vPidMYbAY3g==", "ANR9rFxNayM=") ---> ' + decrypt.a("ZLUR2jUmRVB5pwnJMWMvRniXEc0vPidMYbAY3g==\n", "ANR9rFxNayM=\n"));
+    console.log('("vXTzNkGJf4SwO8Y7DpZtprh04TId", "1xWFV2/lHuo=") ---> ' + decrypt.a("vXTzNkGJf4SwO8Y7DpZtprh04TId\n", "1xWFV2/lHuo=\n"));
+    console.log(' "5odynUmnRB3lnw==", "iugT+SzDF3U="  ----->  ' + decrypt.a("5odynUmnRB3lnw==\n", "iugT+SzDF3U=\n"));
+    console.log(' "MAz+jlPwGQQzOuKtS7lUKA==", "V2mKwjyRfWE="  ----->  ' + decrypt.a("MAz+jlPwGQQzOuKtS7lUKA==\n", "V2mKwjyRfWE=\n"));
+    console.log(' "Vw6mvnKpr65UFo60Y6iOqFoNk7N6qA==", "O2HH2hfN/MY="  ----->  ' + decrypt.a("Vw6mvnKpr65UFo60Y6iOqFoNk7N6qA==\n", "O2HH2hfN/MY=\n"));
+    console.log(' "tndG+5r9sE21QVrYgtW6XLRgXNaZyL1FtDob/Q==", "0RIyt/Wc1Cg="  ----->  ' + decrypt.a("tndG+5r9sE21QVrYgtW6XLRgXNaZyL1FtDob/Q==\n", "0RIyt/Wc1Cg=\n"));
+    console.log(' "jwkx3RbwEUC7OzP7", "9VpennqRYjM="  ----->  ' + decrypt.a("jwkx3RbwEUC7OzP7\n", "9VpennqRYjM=\n"));
+    console.log(' "z5V6L9+gBWjJg3077aIjLIG8ZBT6rmloyZ5pWt+7NG3GlzU=", "qPAOdYzPRgQ="  ----->  ' + decrypt.a("z5V6L9+gBWjJg3077aIjLIG8ZBT6rmloyZ5pWt+7NG3GlzU=\n", "qPAOdYzPRgQ=\n"));
+    console.log(' "stPSAFTQV32sztwgVA==", "yIC9TTGkPxI="  ----->  ' + decrypt.a("stPSAFTQV32sztwgVA==\n", "yIC9TTGkPxI=\n"));
+    console.log(' "OLuaPophLqYrtoEAl28Opnf3og64eALsM7+AA/ZdF7E2sIlf", "X97uZNkOY8M="  ----->  ' + decrypt.a("OLuaPophLqYrtoEAl28Opnf3og64eALsM7+AA/ZdF7E2sIlf\n", "X97uZNkOY8M=\n"));
+    console.log(' "28CjPcUeXeb98qEb", "s5PMfql/LpU="  ----->  ' + decrypt.a("28CjPcUeXeb98qEb\n", "s5PMfql/LpU=\n"));
+    console.log(' "BqzpD2/1wT8Auu4JXffne0iF9yZK+60/AKf6aG/u8DoPrqY=", "YcmdRzyaglM="  ----->  ' + decrypt.a("BqzpD2/1wT8Auu4JXffne0iF9yZK+60/AKf6aG/u8DoPrqY=\n", "YcmdRzyaglM=\n"));
+    console.log(' "huVadCSWTCWK+FRUJA==", "7rY1OUHiJEo="  ----->  ' + decrypt.a("huVadCSWTCWK+FRUJA==\n", "7rY1OUHiJEo=\n"));
+    console.log(' "4MORnH5ZDK/zzoqwY1csr6+Pqb5MQCDl68eLswJlNbjuyILv", "h6bl1C02Qco="  ----->  ' + decrypt.a("4MORnH5ZDK/zzoqwY1csr6+Pqb5MQCDl68eLswJlNbjuyILv\n", "h6bl1C02Qco=\n"));
+    console.log(' "5Y7FLwcQMZb2j+ssEgYOtPKfwC8XOxyU8g==", "l+uoQHN1ffk="  ----->  ' + decrypt.a("5Y7FLwcQMZb2j+ssEgYOtPKfwC8XOxyU8g==\n", "l+uoQHN1ffk=\n"));
+    console.log(' "Yc396husRxVj5ObZGoJEAHXbxN0KqUcFSMnk3VboZAtn3uiXEqBGBin7/coXr09a", "BqiJuH7BKGE="  ----->  ' + decrypt.a("Yc396husRxVj5ObZGoJEAHXbxN0KqUcFSMnk3VboZAtn3uiXEqBGBin7/coXr09a\n", "BqiJuH7BKGE=\n"));
+    console.log(' "nzBuc7FqTkeMJnBSpGJo", "7VUDHMUPDSs="  ----->  ' + decrypt.a("nzBuc7FqTkeMJnBSpGJo\n", "7VUDHMUPDSs=\n"));
+    console.log(' "psqvoUwpDCWk7LeSWjctMKzK89plLgInoIC3kkcjTAK13bKdTn8=", "wa/b8ylEY1E="  ----->  ' + decrypt.a("psqvoUwpDCWk7LeSWjctMKzK89plLgInoIC3kkcjTAK13bKdTn8=\n", "wa/b8ylEY1E=\n"));
+    console.log(' "MbPPFZ8Z1dY1r/cfuBTr1g==", "WMCDev59h7M="  ----->  ' + decrypt.a("MbPPFZ8Z1dY1r/cfuBTr1g==\n", "WMCDev59h7M=\n"));
+    console.log(' "SNzBumksOfxMwPmwTiEH/AmG1w==", "Ia+N1QhIa5k="  ----->  ' + decrypt.a("SNzBumksOfxMwPmwTiEH/AmG1w==\n", "Ia+N1QhIa5k=\n"));
+    console.log(' "Xt544Xo=", "P7oyjxO5/6A="  ----->  ' + decrypt.a("Xt544Xo=\n", "P7oyjxO5/6A=\n"));
+    console.log(' "RxLGNFWl0IUIXv4fUJnfw0wW3BIevMqeSRnVTg==", "IHeydTHvvuw="  ----->  ' + decrypt.a("RxLGNFWl0IUIXv4fUJnfw0wW3BIevMqeSRnVTg==\n", "IHeydTHvvuw=\n"));
+    console.log(' "HhoDfysqNCgfADFz", "d2lFFllZQH8="  ----->  ' + decrypt.a("HhoDfysqNCgfADFz\n", "d2lFFllZQH8=\n"));
+    console.log(' "anejrkwGA2JrbZGiFlwt", "AwTlxz51dzU="  ----->  ' + decrypt.a("anejrkwGA2JrbZGiFlwt\n", "AwTlxz51dzU=\n"));
+    console.log(' "uhSVUw==", "3HbcNzaPKdg="  ----->  ' + decrypt.a("uhSVUw==\n", "3HbcNzaPKdg=\n"));
+    console.log(' "FBMxyfBp1l1aOi/u5EGdGRIYIqDBVMAcHRF+", "c3ZFj5IgsnU="  ----->  ' + decrypt.a("FBMxyfBp1l1aOi/u5EGdGRIYIqDBVMAcHRF+\n", "c3ZFj5IgsnU=\n"));
+})
+
+// ("amE3dqE8", "VhVfH9ICoFA="); ---> <this>
+// ("COnCTTAW", "NJ2qJEMo4uc="); ---> <this>
+// ("COnCTTAW", "NJ2qJEMo4uc="); ---> <this>
+// ("ujAIpnh+Ig==", "ggc6l0FGFSQ="); ---> 8721987
+// ("TTnQWohsC2lf", "LF+PKfwNfxw="); ---> af_status
+// ("3XfcZGNPWg==", "kgW7BQ0mOS8="); ---> Organic
+// (("q293bGuN", "wAUWHw//KyU=")); ---> kjasdr
+// ("f+dYxNWWVr5G+VrU0YE=", "GYAyp7PkIck="); ---> fgjcfrww_yhsbe
+// ("YDdZ8WQTwS5gNw==", "DVItmQt3j08="); ---> methodName
+// ("wy232GUQqULBGLzJbQY=", "s0zFuQh13Sc="); ---> parameterTypes
+// ("T9fJh9qA", "Obal8r/zc78="); ---> values
+// ("UoZyU3aTVpJejHJTUJ9bxXOSfhNh", "POIafRL7Mbw="); ---> ndh.dhg.bnh.BdjyOpdns
+// ("XzWb1WeZOdU=", "PlbvvBHwTaw="); ---> activity
+// ("8QA43fWQpGHxGiCRt5blbP4GIJGhnOVh8Bt536CfqS/rDCTU9ZKra+0aPdX7krV/sTQ3xbyFrHvmODXftJSgfQ==", "n3VUsdXzxQ8=")); ---> null cannot be cast to non-null type android.app.ActivityManager
+// ("D1OQvFtadHsGUbeLXEJzcQ1FzMAAGjM=", "aDbk7i40GhI="); ---> getRunningServices(...)
+// ("0OVeSAs00A==", "o4AsPmJXtTE="); ---> service
+// ("ibswbh06WiaJ9moTR2E=", "7t5EPWlIM0g="); ---> getString(...)
+// ("lkBiBpTx9lLaHA==", "9DULavDZ2Hw="); ---> build(...)
+// ("dPbgCQ==", "F5eMZeFIDrY="); ---> call
+// ("vgknL1EDmh/yVQ==", "3HxOQzUrtDE="); ---> build(...)
+// ("UoZyU3aTVpJejHJTUJ9bxXOSfhNh", "POIafRL7Mbw=") ---> ndh.dhg.bnh.BdjyOpdns
+// ("XzWb1WeZOdU=", "PlbvvBHwTaw=") ---> activity
+// ("D1OQvFtadHsGUbeLXEJzcQ1FzMAAGjM=", "aDbk7i40GhI=") ---> getRunningServices(...)
+// ("ZLUR2jUmRVB5pwnJMWMvRniXEc0vPidMYbAY3g==", "ANR9rFxNayM=") ---> dalvik.system.DexClassLoader
+// ("w/0h5ymh9JvA7jPsA7bMjts=", "rZ9WglzCq+w=") ---> nbweuc_wmqen_tgbv
+// ("6NfM+S/LclzrxN7yBc1JSOA=", "hrW7nFqoLSs=") ---> nbweuc_wmqen_edcf
+// ("upUB2YTjyhKrmxGl8vHoWZ+5PJE=", "+9BS9sehiT0=") ---> AES/CBC/PKCS5Padding
+// ("UOgD", "Ea1Q/ZFlymQ=")) ---> AES
+// ("aSC+c9H/bQ==", "BlfUEb+KHxI=") ---> owjbnur
+// ("ZLUR2jUmRVB5pwnJMWMvRniXEc0vPidMYbAY3g==", "ANR9rFxNayM=") ---> dalvik.system.DexClassLoader
+// ("vXTzNkGJf4SwO8Y7DpZtprh04TId", "1xWFV2/lHuo=") ---> java.lang.ClassLoader
+//  "5odynUmnRB3lnw==", "iugT+SzDF3U="  ----->  loadedShow
+//  "MAz+jlPwGQQzOuKtS7lUKA==", "V2mKwjyRfWE="  ----->  getLoadedShow()I
+//  "Vw6mvnKpr65UFo60Y6iOqFoNk7N6qA==", "O2HH2hfN/MY="  ----->  loadedShowInternalTime
+//  "tndG+5r9sE21QVrYgtW6XLRgXNaZyL1FtDob/Q==", "0RIyt/Wc1Cg="  ----->  getLoadedShowInternalTime()J
+//  "jwkx3RbwEUC7OzP7", "9VpennqRYjM="  ----->  zSoClassName
+//  "z5V6L9+gBWjJg3077aIjLIG8ZBT6rmloyZ5pWt+7NG3GlzU=", "qPAOdYzPRgQ="  ----->  getZSoClassName()Ljava/lang/String;
+//  "stPSAFTQV32sztwgVA==", "yIC9TTGkPxI="  ----->  zSoMethodName
+//  "OLuaPophLqYrtoEAl28Opnf3og64eALsM7+AA/ZdF7E2sIlf", "X97uZNkOY8M="  ----->  getZSoMethodName()Ljava/lang/String;
+//  "28CjPcUeXeb98qEb", "s5PMfql/LpU="  ----->  hSoClassName
+//  "BqzpD2/1wT8Auu4JXffne0iF9yZK+60/AKf6aG/u8DoPrqY=", "YcmdRzyaglM="  ----->  getHSoClassName()Ljava/lang/String;
+//  "huVadCSWTCWK+FRUJA==", "7rY1OUHiJEo="  ----->  hSoMethodName
+//  "4MORnH5ZDK/zzoqwY1csr6+Pqb5MQCDl68eLswJlNbjuyILv", "h6bl1C02Qco="  ----->  getHSoMethodName()Ljava/lang/String;
+//  "5Y7FLwcQMZb2j+ssEgYOtPKfwC8XOxyU8g==", "l+uoQHN1ffk="  ----->  remoteLoadClassMethodName
+//  "Yc396husRxVj5ObZGoJEAHXbxN0KqUcFSMnk3VboZAtn3uiXEqBGBin7/coXr09a", "BqiJuH7BKGE="  ----->  getRemoteLoadClassMethodName()Ljava/lang/String;
+//  "nzBuc7FqTkeMJnBSpGJo", "7VUDHMUPDSs="  ----->  remoteClassName
+//  "psqvoUwpDCWk7LeSWjctMKzK89plLgInoIC3kkcjTAK13bKdTn8=", "wa/b8ylEY1E="  ----->  getRemoteClassName()Ljava/lang/String;
+//  "MbPPFZ8Z1dY1r/cfuBTr1g==", "WMCDev59h7M="  ----->  isLoadRemoteFile
+//  "SNzBumksOfxMwPmwTiEH/AmG1w==", "Ia+N1QhIa5k="  ----->  isLoadRemoteFile()Z
+//  "Xt544Xo=", "P7oyjxO5/6A="  ----->  adJni
+//  "RxLGNFWl0IUIXv4fUJnfw0wW3BIevMqeSRnVTg==", "IHeydTHvvuw="  ----->  getAdJni()Ljava/lang/String;
+//  "HhoDfysqNCgfADFz", "d2lFFllZQH8="  ----->  isFirstWhite
+//  "anejrkwGA2JrbZGiFlwt", "AwTlxz51dzU="  ----->  isFirstWhite()Z
+//  "uhSVUw==", "3HbcNzaPKdg="  ----->  fbId
+//  "FBMxyfBp1l1aOi/u5EGdGRIYIqDBVMAcHRF+", "c3ZFj5IgsnU="  ----->  getFbId()Ljava/lang/String;
+
+
+
